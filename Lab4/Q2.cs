afb04aad1cf21e8ae7bd80fd6b321e1ba2ae6225
Panagoda P R K <136779449+PanagodaPRK@users.noreply.github.com>
2024-02-25 22:27:18 +0530

    Add files via upload

---
+++ Lab4/Q2.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Lab4
+//{
+//    class Employee
+//    {
+//        public int employeeID { get; }
+//        public string FullName { get; set; }
+//        public double Salary { get; private set; }
+
+//        public Employee(int ID, string name, double salary)
+//        {
+//            employeeID = ID;
+//            FullName = name;
+//            Salary = salary;
+//        }
+
+//        public void DisplayEmployeeInfo()
+//        {
+//            Console.WriteLine("Employee ID: " + employeeID);
+//            Console.WriteLine("Full Name: " + FullName);
+//            Console.WriteLine("Salary: " + Salary);
+//        }
+//    }
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Employee employee1 = new Employee(101, "John Doe", 50000);
+//            Console.WriteLine("Employee ID: " + employee1.employeeID);
+//            employee1.FullName = "Charles Dane";
+
+//            //employee.Salary = 60000;
+//            // Observations:
+//            // This results in an error because the Salary property is private. This prevents external code from directly modifying the salary.
+//            Console.ReadLine();
+//        }
+//    }
+//}
---
+++ Lab4/Q3.cs
@@ -0,0 +1,65 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Lab4
+//{
+//    public class Product
+//    {
+//        private int productId;
+//        private string productName;
+//        private double price;
+//        private int quantityInStock;
+
+//        public Product(int id, string name, double Price, int quantity)
+//        {
+//            productId = id;
+//            productName = name;
+//            price = Price;
+//            quantityInStock = quantity;
+//        }
+
+//        public void AddProduct(int quantityToAdd)
+//        {
+//            quantityInStock += quantityToAdd;
+//            Console.WriteLine("Product/s added to stock.");
+//        }
+
+//        public void BuyProduct(int quantityToBuy)
+//        {
+//            if (quantityToBuy <= quantityInStock)
+//            {
+//                quantityInStock -= quantityToBuy;
+//                Console.WriteLine("Product/s sold.");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Not enough stock.");
+//            }
+//        }
+
+//        public void DisplayProductDetails()
+//        {
+//            Console.WriteLine("Product Name: " + productName);
+//            Console.WriteLine("Price: " + price);
+//            Console.WriteLine("Quantity in Stock: " + quantityInStock);
+//        }
+//    }
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Product laptop = new Product(101, "Laptop", 800, 10);
+//            laptop.DisplayProductDetails();
+
+//            //laptop.productId = 102;
+
+//            //Comment:
+//            //due to the protection level of "productID" it is not possible to change values from here and would result in an error.
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
